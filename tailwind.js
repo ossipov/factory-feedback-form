4af0613c870f632d58bcfaa940845947f1c86c98
Dmitry <ossipov@gmail.com>
2020-06-28 18:54:32 +0800

    Adding new dependencies and configuring them

---
+++ tailwind.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: [
+        './resources/vue/**/*.vue',
+        './resources/views/**/*.blade.php',
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
